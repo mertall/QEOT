41a22740f81a7f9a20c76ed6f22877b2d27ef884
mridulsar <42987995+mridulsar@users.noreply.github.com>
2019-09-12 07:28:40 -0700

    Add files via upload

--- Parser.py
+++ Parser.py
@@ -1,3 +1,4 @@
+###                             CORENLP                          ###
 #from nltk.parse import CoreNLPParser
 #import os
 #need 7zip java python Stanford Core NLP
@@ -9,55 +10,87 @@
 #java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer \
 #-preload tokenize,ssplit,pos,lemma,ner,parse,depparse \
 #-status_port 9000 -port 9000 -timeout 15000 
-def parse_text_stanford():
-    folder = os.listdir(r'C:\Users\mridu\OneDrive\Documents\PythonScripts\VMware\QEOT\cloud_documentation')
-    for efile in folder :
-        with open(efile) as f :
-            data = f.read()
-            parser = CoreNLPParser(url='http://localhost:9000')
-            pos_tagger = CoreNLPParser(url='http://localhost:9000', tagetype='pos')
-            ner_tagger = CoreNLPParser(url='http://localhost:9000', tagtype = 'ner')
-            tokens = parser.tokenize(data)
-            pos = pos_tagger.tag(data)
-            ner = ner_tagger.tag(data)
-            print(tokens)
-            print(pos)
-            print(ner)
+#def parse_text_stanford():
+#    folder = os.listdir(r'C:\Users\mridu\OneDrive\Documents\PythonScripts\VMware\QEOT\cloud_documentation')
+#    for efile in folder :
+#        with open(efile) as f :
+#            data = f.read()
+#            parser = CoreNLPParser(url='http://localhost:9000')
+#            pos_tagger = CoreNLPParser(url='http://localhost:9000', tagetype='pos')
+#            ner_tagger = CoreNLPParser(url='http://localhost:9000', tagtype = 'ner')
+#            tokens = parser.tokenize(data)
+#            pos = pos_tagger.tag(data)
+#            ner = ner_tagger.tag(data)
+#            print(tokens)
+#            print(pos)
+#            print(ner)
 #parse_text()
 
-#Cannot conect to stanford server for some reason... lets utilize spacy then
+#Cannot conect to stanford server for some reason... lets utilize spacy then...  I think its because I am using UNIX based station... utilizing linux terminal should let us use the code above!
 
+###                             SPACY                            ###
 import spacy
 import os
 folder = os.listdir(r'C:\Users\mridu\OneDrive\Documents\VMware\cloud_documentation')
-nlp = spacy.load('en_core_web_lg') ## lg lets us use large documents
-tokens = []
-pos = []
-dep = []
-sim = []
-hasvector = []
-vectornorm = []
-isoov = []
-for efile in folder:
-    with open(efile,'r') as f:
-        data = f.read()
-        doc = nlp(data)
-        for token in doc:
-            tokens = token.append(token.text)
-            pos = pos.append(token.pos_)
-            dep = dep.append(token.dep_)
-            hasvector = hasvector.append(token.has_vector)
-            vectornorm = vectornorm.append(token.vector_norm)
-            isoov = isoov.append(token.is_oov)
-        cloud_dict = {}
-        i = 1
-        cloud_dict[i] = [tokens,pos,dep,hasvector,vectornorm,isoov]
-        i = i + 1
 
-##NEXT step find similarity between docs
-## compare all vector norms with each other
-## compare has vectors with each others
-## what to do i do with dependancy 
+##input must be a folder
+def grab_doc_info(folder):
+    j = 0 ##count documents in folder, 0...N
+    nlp = spacy.load('en_core_web_lg') ## lg lets us use large documents
+    tokens = [] ##store all tokens from all the documents
+    pos = [] ##store all parts of speech for each token MUST CHECK if POS can't be labeled does it still output something... to make sure we map POS to each token that exists wihthin the documents
+    dep = [] ##store dependnancy of each token MUST CHECK how dependnancy is found... is it token wise?
+    sim = [] ##store similarity of each token MUST CHECK how similarity is found... is it toke wise?
+    hasvector = [] ##store if token can be vectorized or not... using some sort of norm equation, i think it was the forbenuis equation that was mentioned in the documentation
+    vectornorm = [] ##stores scalar values of vector norm... useful info
+    isoov = [] ##if toke is out of vocabulary False if in vocabulary True, both being boolean values
+    cloud_dict = {} ##dictionary to map each document, numbered numerically from 0...N, to their the respective characteristics of the document, listed above... within each of theses characteristic lists there are sublists whose position corresponds directly to the document number
+    for efile in folder: ##go through each file in the folder
+        with open(efile,'r') as f: ##setting file attribute which is to be utilized
+            data = f.read() 
+            doc = nlp(data) ##spacy function, tokenizes our document and makes it ready for us to process
+            ###         DOCUMENT CHARACTERISTICS            ###
+            new_token = [] ##           0
+            new_pos = [] ##             1
+            new_dep = []##              2
+            new_sim = []##              3
+            new_hasvector=[]##          4
+            new_vectornorm=[]##         5
+            new_isoov=[]##              6
+            for tokens in doc:
+                new_token = new_token.append(tokens.text)
+                new_pos = new_pos.append(tokens.pos_)
+                new_dep = new_dep.append(tokens.dep_)
+                new_sim = new_sim.append(tokens.sim_)
+                new_hasvector = new_hasvector.append(tokens.has_vector)
+                new_vectornorm = new_vectornorm.append(tokens.vector_norm)
+                new_isoov = new_isoov.append(tokens.is_oov)
+            tokens = tokens.append(new_token)
+            pos = pos[j].append(new_pos)
+            dep = dep[j].append(new_dep)
+            sim = sim[j].append(new_sim)
+            hasvector = hasvector[j].append(new_hasvector)
+            vectornorm = vectornorm[j].append(new_vectornorm)
+            isoov = isoov[j].append(new_isoov)
+            j = j + 1 ##counts once we have opened a new documents
+    return cloud_dict
 
 
-### what to do with oov... could do something cool... which documents are the hardest to understand...
+def find_depth(cloud_dict):
+## create depednancy tree and then use depth firstsearch counting how deep the treee is 
+
+def understandability(cloud_dict): ##lol, go through each documents and finds ratio of words that are not understandable
+    iter = len(cloud_dict)
+    counter = 0
+    score =[]
+    for i in range(iter):
+        doc = cloud_dict[iter]
+        num_tokens = len(cloud_dict)
+        for j in range(num_tokens):
+            isoov = doc[6]
+            if isoov[j] == 1:
+                counter += 1
+            else:
+                pass
+        score[i] = counter/num_tokens
+    return score
